b021f927d79eea53fae91d4ba3d944a7de3027e4
Steven Moctezuma <stevenalex96@gmail.com>
2021-11-15 22:09:30 -0600

    Create no-anchors.js

---
+++ no-anchors.js
@@ -0,0 +1,20 @@
+// let anchors = document.getElementsByTagName('a');
+// console.log("tester");
+// anchors.forEach(() => {
+
+//     console.log("test");
+// });
+document.body.style.border = "5px solid red";
+console.log("here");
+
+//let anchors = document.getElementsByTagName("a");
+
+
+console.log("here 1");
+
+// anchors.forEach((anchor) => {
+//     console.log("in anchors");
+//     console.log("Anchor: "+ anchor);
+// });
+
+// console.log("here 2");
